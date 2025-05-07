b8a8b4405487109e895de271311515fc85ed84e1
DennisGabriel <dennisgabr745@gmail.com>
2025-05-06 23:40:02 -0300

    initial project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user.id, status: :created
+    else
+      render json: (message: "Error creating user"), status: :unauthorized
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.expect(user: [ :email, :password, :name ])
+    end
+end
